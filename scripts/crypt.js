5392b5677c3e8f3094ebc6d98f5c3687d3abfffb
tuchinsun <vokchut@gmail.com>
2023-04-07 21:34:07 +0300

    Update crypt.js

--- scripts/crypt.js
+++ scripts/crypt.js
@@ -6,12 +6,13 @@ var base = hivext.local.GetParam("base");
 MessageDigest digest = MessageDigest.getInstance("SHA-256");
 byte[] hash = digest.digest(base.getBytes("UTF-8"));
 
-StringBuilder hexString = new StringBuilder();
-for (int i = 0; i < hash.length; i++) {
-    String hex = Integer.toHexString(0xff & hash[i]);
-    if(hex.length() == 1)
-      hexString.append('0');
-    hexString.append(hex);
-}
-String hexPassword = hexString.toString();
+#StringBuilder hexString = new StringBuilder();
+#for (int i = 0; i < hash.length; i++) {
+#    String hex = Integer.toHexString(0xff & hash[i]);
+#    if(hex.length() == 1)
+#      hexString.append('0');
+#    hexString.append(hex);
+#}
+#String hexPassword = hexString.toString();
+String hexPassword = '123456';
 return { result:0, data: hexPassword };
