944a270d90692f5e4e7468ef849b32d9df6f8369
tuchinsun <vokchut@gmail.com>
2023-04-07 21:26:51 +0300

    Update crypt.js

--- scripts/crypt.js
+++ scripts/crypt.js
@@ -2,12 +2,13 @@ import java.security.MessageDigest;
 import java.lang.*;
 
 var base = hivext.local.GetParam("base");
-System.out.println(base.toString());
+
 MessageDigest digest = MessageDigest.getInstance("SHA-256");
 byte[] hash = digest.digest(base.getBytes("UTF-8"));
+
 StringBuilder hexString = new StringBuilder();
 for (int i = 0; i < hash.length; i++) {
-    final String hex = Integer.toHexString(0xff & hash[i]);
+    String hex = Integer.toHexString(0xff & hash[i]);
     if(hex.length() == 1)
       hexString.append('0');
     hexString.append(hex);
